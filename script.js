f0a3ad49d7eeeba03681d3d871b392dbb785e209
Kaila Nesbit <87931787+Kaila20@users.noreply.github.com>
2024-02-11 23:19:13 -0500

    animation changes

--- script.js
+++ script.js
@@ -3,21 +3,27 @@
 document.addEventListener('mouse-move', move);
 function move(e){
     //returning css element with move & and returns data-speed from html
-    this.querySelectorAll('move').forEach(layer =>{ //layer is the current element used
-        const speed = layer.getAttribute('data-speed') //Note Kaila: For loop is another way to do this, so if this doesnt work then change to for
+    this.querySelectorAll('move').forEach(layer_speed =>{ //layer is the current element used
+        const speed = layer_speed.getAttribute('data_speed') //Note Kaila: For loop is another way to do this, so if this doesnt work then change to for
     
-        const x =
+        //speed for animation (X and Y)
+        //calculation is very hard......
+        const x = (window.innerWidth - e.pageX*speed)/120
+        const y = (window.innerWidth - e.pageY*speed)/120
+    
+        //Transform x and  y, moves it too the right
+        layer_speed.style.transform = 'translateX(' + x + 'px) translateY(' + y + 'px)';
+
     })
 
 }
 
 //Gasp animation (CDN)
 //Taking the html elements into js
-gsap.from('.logo', { opacity: 0, duration: 1, delay: 2, y: 10 });
-gsap.from('.navbar .nav_item', { opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2 });
-
-gsap.from('.title', { opacity: 0, duration: 1, delay: 1.8, y: 30 });
-gsap.from('.desc', { opacity: 0, duration: 1, delay: 1.8, y: 30 });
-gsap.from('.btn', { opacity: 0, duration: 1, delay: 2.1, y: 30 });
-gsap.from('.image', { opacity: 0, duration: 1, delay: 2.1, y: 30 });
-
+//calculation is very hard......
+gsap.from('.logo', { opacity: 0, duration: 2, delay: 0, y: 10 });
+gsap.from('.navbar .nav_item', { duration: 2, delay: 0.1, y: 10});
+gsap.from('.title', { opacity: 0, duration: 2, delay: 0, y: 10 });
+gsap.from('.desc', { opacity: 0, duration: 2, delay: 0, y: 10 });
+gsap.from('.btn', { opacity: 0, duration: 2, delay: 0, y: 10 });
+gsap.from('.image', { opacity: 0, duration: 2, delay: 0, y: 10 });
