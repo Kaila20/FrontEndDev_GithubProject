5e4f7e9c4f96b6fa807b2af8b9869c5b2c7264c2
Kaila Nesbit <87931787+Kaila20@users.noreply.github.com>
2024-02-11 22:21:10 -0500

    Js changes

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 
+//Adding event listener when clciked, getting the data-speed element/attribute 
+document.addEventListener('mouse-move', move);
+function move(e){
+    //returning css element with move & and returns data-speed from html
+    this.querySelectorAll('move').forEach(layer =>{ //layer is the current element used
+        const speed = layer.getAttribute('data-speed') //Note Kaila: For loop is another way to do this, so if this doesnt work then change to for
+    
+        const x =
+    })
+
+}
+
 //Gasp animation (CDN)
-gasp.from('.logo',(opacity:0, duration: 1, delay: 2, y:10))
+//Taking the html elements into js
+gsap.from('.logo', { opacity: 0, duration: 1, delay: 2, y: 10 });
+gsap.from('.navbar .nav_item', { opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2 });
+
+gsap.from('.title', { opacity: 0, duration: 1, delay: 1.8, y: 30 });
+gsap.from('.desc', { opacity: 0, duration: 1, delay: 1.8, y: 30 });
+gsap.from('.btn', { opacity: 0, duration: 1, delay: 2.1, y: 30 });
+gsap.from('.image', { opacity: 0, duration: 1, delay: 2.1, y: 30 });
+
